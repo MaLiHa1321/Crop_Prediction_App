b1bc953b3ccfe312946ac21bfc3dcb1fa7f20648
MaLiHa1321 <malihasultana2001@gmail.com>
2025-11-13 19:22:19 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods=["POST"])
+def predict():
+
+    nitrogen = float(request.form['Nitrogen'])
+    phosphorus = float(request.form['Phosporash'])
+    potassium = float(request.form['potassium'])
+    temperature = float(request.form['temperature'])
+    humidity = float(request.form['humidity'])
+    ph = float(request.form['ph'])
+    rainfall = float(request.form['rainfall'])
+
+
+    feature = np.array([[nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]])
+    prediction = model.predict(feature)
+
+
+    return render_template("index.html", prediction_txt=f"The predicted crop is {prediction[0]}")
+
+if __name__=="__main__":
+    flask_app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+import pickle
+
+data = pd.read_csv(r"D:\Flask Project\crop_prediction\Crop_recommendation.csv")
+
+data.head(5)
+data.shape
+data.isnull().sum()
+x = data.iloc[:,:-1] #feature
+y = data.iloc[:,-1] #labels
+
+X_train, X_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+X_train.head()
+y_train.head()
+
+model = RandomForestClassifier()
+model.fit(X_train,y_train)
+
+pickle.dump(model, open("model.pkl", "wb"))
+prediction = model.predict(X_test)
+
+accuracy = model.score(X_test,y_test)
+
+print("Accuracy:", accuracy)
