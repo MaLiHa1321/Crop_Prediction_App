87ffeb6a03b63d8076519817ec86f05220f57bf7
MaLiHa1321 <malihasultana2001@gmail.com>
2025-11-16 15:58:56 +0600

    authentication are added

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
 import pickle
+from config import MONGO_URI, SECRET_KEY
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 flask_app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
+
+flask_app.config["MONGO_URI"] = MONGO_URI
+flask_app.secret_key = SECRET_KEY
+
+print("MONGO URI =", MONGO_URI)
+
+mongo = PyMongo(flask_app)
+db = mongo.db
+
+@flask_app.route("/testdb")
+def testdb():
+    try:
+        result = db.users.find_one()
+        return str(result)
+    except Exception as e:
+        return str(e)
 
 
 @flask_app.route("/")
@@ -36,6 +55,62 @@ def predict():
 
 
     return render_template("index.html", prediction_txt=f"The predicted crop is {prediction[0]}")
+
+
+
+@flask_app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+     
+        if db.users.find_one({"email": email}):
+            return "Email already exists!"
+
+        hashed_pass = generate_password_hash(password)
+        db.users.insert_one({
+            "name": name,
+            "email": email,
+            "password": hashed_pass
+        })
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+
+@flask_app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = db.users.find_one({"email": email})
+        if user and check_password_hash(user["password"], password):
+            session["user_id"] = str(user["_id"])
+            session["name"] = user["name"]
+            return redirect(url_for("dashboard"))
+        else:
+            return "Invalid email or password!"
+
+    return render_template("login.html")
+
+
+@flask_app.route("/dashboard")
+def dashboard():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    user_predictions = list(db.predictions.find({"user_id": session['user_id']}))
+    return render_template("dashboard.html", name=session["name"], predictions=user_predictions)
+
+
+@flask_app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()  
+    return redirect(url_for('home'))
 
 if __name__=="__main__":
     flask_app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,12 @@
+# Crop_prediction
+# umft8RIeY7Jk6gp7
+# const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.ntnzcww.mongodb.net/?appName=Cluster0";
+# SECRET_KEY = "mysecretkey123"
+
+# MONGO_URI = "mongodb+srv://Crop_prediction:umft8RIeY7Jk6gp7@cluster0.ntnzcww.mongodb.net/?appName=Cluster0"
+# SECRET_KEY = "mysecretkey123"
+# MongoDB credentials
+MONGO_URI = "mongodb+srv://Crop_prediction:umft8RIeY7Jk6gp7@cluster0.ntnzcww.mongodb.net/cropDB?retryWrites=true&w=majority"
+
+# Flask secret key
+SECRET_KEY = "mysecretkey123"
